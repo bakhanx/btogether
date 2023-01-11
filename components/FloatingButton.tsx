eadcc6d89c9cd27c9c18275a4dd5c1139aaba3f1
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-01-11 16:20:09 +0900

    Layout, Floating button

---
+++ components/FloatingButton.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+interface FloatingButton {
+  children: React.ReactNode;
+  href: string;
+}
+
+export default function FloatingButton({ children, href }: FloatingButton) {
+  return (
+    <Link href={href}>
+      <div className="fixed bottom-24 right-5 flex aspect-square w-14 cursor-pointer items-center justify-center rounded-full border-0 border-transparent bg-blue-400 text-white shadow-xl transition-colors hover:bg-blue-500 ">
+        {children}
+      </div>
+    </Link>
+  );
+}
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -21,9 +21,12 @@ export default function Layout({
   };
   return (
     <div>
-      <div className="max-w-x1 fixed top-0 flex h-12 w-full items-center justify-center border-b bg-white px-10 text-lg font-medium text-gray-800">
+
+      {/* 상단 탭 */}
+      <div className="max-w-x1 fixed top-0 flex h-12 w-full items-center justify-center border-b bg-blue-400 px-10 text-lg font-medium text-white">
+        {/* 뒤로가기 */}
         {canGoBack ? (
-          <button onClick={onClick}>
+          <button onClick={onClick} className="absolute left-4">
             <svg
               className="h-6 w-6"
               fill="none"
@@ -40,20 +43,27 @@ export default function Layout({
             </svg>
           </button>
         ) : null}
+        {/* 타이틀 */}
         {title ? (
-          <span className={cls(canGoBack ? "mx-auto" : "", "")}></span>
+          <span className={cls(canGoBack ? "mx-auto" : "", "")}>{title}</span>
         ) : null}
       </div>
+
+      {/* 메인 컨텐츠 */}
       <div className={cls("pt-12", hasTabBar ? "pb-24" : "")}>{children}</div>
+
+      {/* 하단 탭 */}
       {hasTabBar ? (
-        <nav>
-            {/* Link 하위에 a태그 사용시 Link에 legacyBehavior 추가 */}
+        <nav className="max-w-x1 fixed bottom-0 flex w-full justify-between border-t bg-gray-50 px-10 pb-5 pt-3 text-xs text-gray-600">
+          {/* Link 하위에 a태그 사용시 Link에 legacyBehavior 추가 */}
+
+          {/* 홈 */}
           <Link href="/">
             <div
               className={cls(
                 "flex flex-col items-center space-y-2",
                 router.pathname === "/"
-                  ? "text-orange-500"
+                  ? "text-blue-400"
                   : "transition-colors hover:text-gray-500"
               )}
             >
@@ -74,10 +84,119 @@ export default function Layout({
               <span>홈</span>
             </div>
           </Link>
+          {/* 이웃소식 */}
+          <Link href="/community">
+            <div
+              className={cls(
+                "flex flex-col items-center space-y-2",
+                router.pathname === "/community"
+                  ? "text-blue-400"
+                  : "transition-colors hover:text-gray-500"
+              )}
+            >
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
+                ></path>
+              </svg>
+              <span>이웃소식</span>
+            </div>
+          </Link>
+
+          {/* 채팅 */}
+          <Link href="/chats">
+            <div
+              className={cls(
+                "flex flex-col items-center space-y-2",
+                router.pathname === "/chats"
+                  ? "text-blue-400"
+                  : "transition-colors hover:text-gray-500"
+              )}
+            >
+              {" "}
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                ></path>
+              </svg>
+              <span>채팅</span>
+            </div>
+          </Link>
+          {/* 라이브 */}
+          <Link href="/live">
+            <div
+              className={cls(
+                "flex flex-col items-center space-y-2",
+                router.pathname === "/lives"
+                  ? "text-blue-400"
+                  : "transition-colors hover:text-gray-500"
+              )}
+            >
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
+                ></path>
+              </svg>
+              <span>라이브</span>
+            </div>
+          </Link>
+          {/* 프로필 */}
+          <Link href="/profile">
+            <div
+              className={cls(
+                "flex flex-col items-center space-y-2",
+                router.pathname === "/profile"
+                  ? "text-blue-400"
+                  : "transition-colors hover:text-gray-500"
+              )}
+            >
+              {" "}
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                ></path>
+              </svg>
+              <span>마이페이지</span>
+            </div>
+          </Link>
         </nav>
-      ) : (
-        ""
-      )}
+      ) : null}
     </div>
   );
 }
