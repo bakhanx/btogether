cee7e700050d8314b5d0998193b3e71cf3179e76
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-06-12 17:20:27 +0900

    port 587 TLS

--- libs/server/email.ts
+++ libs/server/email.ts
@@ -5,7 +5,7 @@ const sendMail = async (toMail: string, payload: string) => {
   const smtpTransport = nodemailer.createTransport({
     service: "Naver",
     host: "smtp.naver.com",
-    port: 465,
+    port: 587,
     secure:false,
     requireTLS: true,
     auth: {
@@ -24,23 +24,30 @@ const sendMail = async (toMail: string, payload: string) => {
     text: `인증 코드 : ${payload}`,
     html: `<strong>인증 코드 : ${payload}</strong>`,
   };
+  console.log("smtpTransport : ",smtpTransport)
+  console.log("mailOptions : ", mailOptions);
 
   await new Promise((resolve,  reject) =>{
     return smtpTransport.sendMail(
       mailOptions,
       (error, response) => {
+        console.log("전송중");
         if (error) {
           reject(error);
+          console.log("error : ", error);
           return null;
         } else {
          resolve(response)
+         console.log("response : ", response);
           return null;
         }
+        
       }
     );
   })
-
+  console.log("smtp close 시작");
   smtpTransport.close();
+  console.log("smtp close 완료")
 };
 
 export default sendMail;
