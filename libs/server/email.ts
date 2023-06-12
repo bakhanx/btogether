08e20deab05478d5301278e60dac1d9b89d6abd6
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-06-12 22:15:52 +0900

    토큰 메일 전송

--- libs/server/email.ts
+++ libs/server/email.ts
@@ -26,27 +26,20 @@ const sendMail = async (toMail: string, payload: string) => {
   };
 
   await new Promise((resolve,  reject) =>{
-    console.log("promise....")
     smtpTransport.sendMail(
       mailOptions,
       (error, response) => {
-        console.log("전송중");
         if (error) {
           reject(error);
-          console.log("error : ", error);
           return null;
         } else {
          resolve(response)
-         console.log("response : ", response);
           return null;
         }
-        
       }
     );
   })
-  console.log("smtp close 시작");
   smtpTransport.close();
-  console.log("smtp close 완료")
 };
 
 export default sendMail;
