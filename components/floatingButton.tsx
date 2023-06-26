896ce991e3ab2e9bdc937013d13bf55a32b5267f
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-06-26 21:46:54 +0900

    버튼 클릭 영역 수정

--- components/floatingButton.tsx
+++ components/floatingButton.tsx
@@ -7,9 +7,9 @@ interface FloatingButton {
 export default function FloatingButton({ writeBtnPath }: FloatingButton) {
   return (
     <div className="relative bottom-24 z-40 flex max-w-screen-xl   justify-center border-none">
-      <div className="bottom-24 flex h-16 w-16 cursor-pointer items-center justify-center rounded-full border-0 border-transparent bg-blue-500 text-white shadow-xl transition-colors  hover:bg-blue-600 ">
-        {writeBtnPath === "product" && (
-          <Link href={"products/upload"}>
+      {writeBtnPath === "product" && (
+        <Link href={"products/upload"}>
+          <div className="bottom-24 flex h-16 w-16 cursor-pointer items-center justify-center rounded-full border-0 border-transparent bg-blue-500 text-white shadow-xl transition-colors  hover:bg-blue-600 ">
             <svg
               className="h-6 w-6"
               xmlns="http://www.w3.org/2000/svg"
@@ -25,10 +25,13 @@ export default function FloatingButton({ writeBtnPath }: FloatingButton) {
                 d="M12 6v6m0 0v6m0-6h6m-6 0H6"
               />
             </svg>
-          </Link>
-        )}
-        {writeBtnPath === "story" && (
-          <Link href={"community/write"}>
+          </div>
+        </Link>
+      )}
+
+      {writeBtnPath === "story" && (
+        <Link href={"community/write"}>
+          <div className="bottom-24 flex h-16 w-16 cursor-pointer items-center justify-center rounded-full border-0 border-transparent bg-blue-500 text-white shadow-xl transition-colors  hover:bg-blue-600 ">
             <svg
               className="h-6 w-6"
               fill="none"
@@ -43,9 +46,9 @@ export default function FloatingButton({ writeBtnPath }: FloatingButton) {
                 d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
               />
             </svg>
-          </Link>
-        )}
-      </div>
+          </div>
+        </Link>
+      )}
     </div>
   );
 }
