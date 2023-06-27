fc38b14472b6a89ba80f522df1254146f5e1f883
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-06-27 20:50:32 +0900

    spinner

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
-import Spinner from "../public/loadingDuck.gif";
+import Loader from "../public/Spinner.svg";
 
 export default function Loading() {
   return (
-    <div className="flex justify-center">
-      <div>
-        <Image src={Spinner} alt="" width={80} height={80}></Image>
-      </div>
+    <div className="mt-10 flex justify-center">
+      <Image src={Loader} alt="" width={80} height={80}></Image>
     </div>
   );
 }
