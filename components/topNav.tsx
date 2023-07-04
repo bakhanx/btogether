cfc24e44a35983c7999dec530f23a07d59af492b
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-07-04 16:35:02 +0900

    레이아웃 투명 처리

--- components/topNav.tsx
+++ components/topNav.tsx
@@ -20,18 +20,18 @@ export default function TopNav({ menuProps: props }: MenuProps) {
     <div
       className={cls(
         props.type === "Product"
-          ? "bg-gradient-to-r from-cyan-500 via-blue-500 to-purple-500 opacity-40"
+          ? "bg-gradient-to-r from-cyan-500/25 via-blue-500/25 to-purple-500/25 "
           : props.type === "Story"
           ? "bg-gradient-to-r from-pink-500 via-amber-500 to-yellow-500"
           : "",
-        "fixed top-0 z-10 flex h-12 w-full max-w-screen-lg  items-center justify-between bg-blue-300 px-5 text-lg font-medium text-white "
+        "fixed top-0 z-10 flex h-12 w-full max-w-screen-lg  items-center justify-between  px-5 text-lg font-medium text-white"
       )}
     >
       {/* 뒤로가기 */}
 
-      <button onClick={onBack} className="">
+      <button onClick={onBack} className="backdrop-opacity-100">
         <svg
-          className="h-6 w-6  "
+          className="h-6 w-6"
           fill="none"
           stroke="currentColor"
           viewBox="0 0 24 24"
