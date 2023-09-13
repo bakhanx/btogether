931a4f6bc5bc23be25251f3dff37bede3951c4a1
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-09-13 21:57:19 +0900

    배너 디자인

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,16 +10,13 @@ const Home: NextPage = () => {
   return (
     <Layout hasTabBar mainTitle seoTitle="이웃과 함께하는" pathName="Product">
       <Suspense fallback={<Loading />}>
-
-        <div className="px-4 py-4">
-          
-            <div className="bg-gradient-to-r from-blue-600 via-blue-500 to-blue-300 text-white p-5 rounded-md space-y-10 ">
-              <div className="text-2xl">이웃과 함께하는 비투게더</div>
-              <div>
-                <div>공동구매, 중고거래, 친목도모 등</div>
-                <div>다양하게 활용해 보세요!</div>
-              </div>
-            
+        <div className="px-4 py-4 ">
+          <div className="bg-gradient-to-tr from-blue-500 via-sky-500 to-sky-400 text-white p-5 rounded-md space-y-10  shadow-violet-400 shadow-md">
+            <div className="text-2xl">이웃과 함께하는 비투게더</div>
+            <div>
+              <div>공동구매, 중고거래, 친목도모 등</div>
+              <div>다양하게 활용해 보세요!</div>
+            </div>
           </div>
         </div>
 
