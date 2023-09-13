91528403891cbdbac6cb58bc92a82871995f5b21
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-09-13 21:35:39 +0900

    메인 배너 디자인 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,20 @@ const Home: NextPage = () => {
   return (
     <Layout hasTabBar mainTitle seoTitle="이웃과 함께하는" pathName="Product">
       <Suspense fallback={<Loading />}>
-        <div className="px-4 py-2">
+
+        <div className="px-4 py-4">
+          
+            <div className="bg-gradient-to-r from-blue-600 via-blue-500 to-blue-300 text-white p-5 rounded-md space-y-10 ">
+              <div className="text-2xl">이웃과 함께하는 비투게더</div>
+              <div>
+                <div>공동구매, 중고거래, 친목도모 등</div>
+                <div>다양하게 활용해 보세요!</div>
+              </div>
+            
+          </div>
+        </div>
+
+        {/* <div className="px-4 py-2">
           <div className="space-y-10 rounded-md bg-blue-400 p-5 text-white">
             <div className="text-2xl">이웃과 함께하는 비투게더</div>
             <div>
@@ -19,7 +32,7 @@ const Home: NextPage = () => {
             </div>
           </div>
           <div></div>
-        </div>
+        </div> */}
 
         <ProductsList />
       </Suspense>
