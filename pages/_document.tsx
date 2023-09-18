7cbe0949818170d701f99d2b9d604ebe6d7919ea
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-09-18 21:10:57 +0900

    kakao cdn 제거

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
-
 export default function Document() {
   return (
     <Html lang="ko">
@@ -13,24 +11,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        {/* <Script
-          src="https://t1.kakaocdn.net/kakao_js_sdk/v2/kakao.min.js"
-          strategy="lazyOnload"
-        />
-
-        <Script
-          src="https://connect.facebook.net/en_US/sdk.js"
-          onLoad={() =>
-            (window.fbAsyncInit = function () {
-              FB.init({
-                appId: "your-app-id",
-                autoLogAppEvents: true,
-                xfbml: true,
-                version: "v16.0",
-              });
-            })
-          }
-        /> */}
       </body>
     </Html>
   );
