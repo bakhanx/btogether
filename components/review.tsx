f695aad7b115977b1ad9aab3dfe5f799673c936e
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-09-21 21:10:18 +0900

    평점 소수점 표시

--- components/review.tsx
+++ components/review.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const Review = () => {
-  const [score, setScore] = useState(5.0);
+  const [score, setScore] = useState(5);
   return (
     <>
       <div className="mt-12">
@@ -10,20 +10,24 @@ const Review = () => {
           <div>
             <h4 className="text-sm font-bold text-gray-800">공구의신</h4>
             {/* 별점 */}
-            <div className="flex items-center">
-              score
-              {Array(score).fill(0).map((_, i) => (
-                <svg
-                  key={i}
-                  className="h-5 w-5 text-yellow-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                </svg>
-              ))}
+            <div className="flex">
+              <div className="flex items-center">
+                {Array(score)
+                  .fill(0)
+                  .map((_, i) => (
+                    <svg
+                      key={i}
+                      className="h-5 w-5 text-yellow-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                  ))}
+              </div>
+              <div className="pl-1">({score.toFixed(1)})</div>
             </div>
           </div>
         </div>
