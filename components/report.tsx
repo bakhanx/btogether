51105b3c91e1eaebe6d392739a2c6b5f0c1c5072
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-09-27 21:36:49 +0900

    신고기능

---
+++ components/report.tsx
@@ -0,0 +1,16 @@
+export default function Report() {
+  return (
+    <>
+      <div>
+        <div>영리/홍보</div>
+        <div>욕설/비하</div>
+        <div>내용반복(도배)</div>
+        <div>개인정보노출</div>
+        <div>음란/선정</div>
+        <div>사기/불법</div>
+        <div>사재기</div>
+        <div>기타</div>
+      </div>
+    </>
+  );
+}
