5e1d0a359785804225ba8622729de2b3512a0ab9
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-09-29 18:30:11 +0900

    신고 UI

--- components/report.tsx
+++ components/report.tsx
@@ -1,15 +1,23 @@
 export default function Report() {
   return (
     <>
-      <div>
-        <div>영리/홍보</div>
-        <div>욕설/비하</div>
-        <div>내용반복(도배)</div>
-        <div>개인정보노출</div>
-        <div>음란/선정</div>
-        <div>사기/불법</div>
-        <div>사재기</div>
-        <div>기타</div>
+      <div className="relative flex justify-center w-full">
+        <div className="center fixed top-48 z-50 flex h-96 w-2/3 max-w-xl items-center justify-center bg-slate-50 opacity-90 text-black border-purple-200 border rounded-md">
+          <div className="flex flex-row w-full justify-around">
+            <div className="flex flex-col gap-y-4">
+              <button className="hover:text-red-700">영리/홍보</button>
+              <button className="hover:text-red-700">욕설/비하</button>
+              <button className="hover:text-red-700">내용반복(도배)</button>
+              <button className="hover:text-red-700">개인정보노출</button>
+            </div>
+            <div className="flex flex-col gap-y-4">
+              <button className="hover:text-red-700">음란/선정</button>
+              <button className="hover:text-red-700">사기/불법</button>
+              <button className="hover:text-red-700">사재기</button>
+              <button className="hover:text-red-700">기타</button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
