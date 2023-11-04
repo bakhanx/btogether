b0ee06ee92ded61e175842a5a3f4bd832bd3a3b6
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-11-04 23:54:23 +0900

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import { NextPage } from "next";
 import Layout from "@components/layout";
-import { SWRConfig } from "swr";
 import { Suspense, lazy } from "react";
 import Loading from "@components/loading";
 
@@ -19,18 +18,6 @@ const Home: NextPage = () => {
             </div>
           </div>
         </div>
-
-        {/* <div className="px-4 py-2">
-          <div className="space-y-10 rounded-md bg-blue-400 p-5 text-white">
-            <div className="text-2xl">이웃과 함께하는 비투게더</div>
-            <div>
-              <div>공동구매, 중고거래, 친목도모 등</div>
-              <div>다양하게 활용해 보세요!</div>
-            </div>
-          </div>
-          <div></div>
-        </div> */}
-
         <ProductsList />
       </Suspense>
     </Layout>
