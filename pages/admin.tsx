9cb2ae4367d6c27ac683abd94498b1cce2f86adb
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-11-16 20:44:05 +0900

    날짜

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -3,6 +3,7 @@ import useSWR from "swr";
 type reportType = {
   reportNum: number;
   reportedUrl: string;
+  updatedAt : string;
 };
 
 const REPORT_CATE = [
@@ -16,18 +17,21 @@ const REPORT_CATE = [
   "기타",
 ];
 
+
+
 const Admin = () => {
   const { data: reportData, isLoading } = useSWR<reportType[]>(`api/report`);
 
   return (
     <>
-      <div></div>
+      <div className="p-2">관리자 페이지</div>
       {reportData?.map((data) => {
         return (
           <>
             <div className="p-2">
               <div>신고내용 : {REPORT_CATE[data?.reportNum]}</div>
               <div>신고경로 : {data?.reportedUrl}</div>
+              <div>신고날짜 : {data?.updatedAt}</div>
             </div>
           </>
         );
