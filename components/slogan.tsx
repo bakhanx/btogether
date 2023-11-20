9aafe036b2e67938ec6a03c9b274583635cfd71d
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-11-20 20:16:26 +0900

    페이지 배너 슬로건

---
+++ components/slogan.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from "next";
+
+const Slogan: NextPage = () => {
+  return (
+    <div className="px-4 py-4 ">
+      <div className="bg-gradient-to-tr from-blue-500 via-sky-500 to-sky-400 text-white p-5 rounded-md space-y-10  shadow-violet-400 shadow-md">
+        <div className="text-2xl">이웃과 함께하는 비투게더</div>
+        <div>
+          <div>공동구매, 중고거래, 친목도모 등</div>
+          <div>다양하게 활용해 보세요!</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slogan;
