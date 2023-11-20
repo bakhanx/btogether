806020e702beb29860ea74faed6e0420defd945a
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-11-20 20:17:01 +0900

    컴포넌트 분리 및 lazyloading 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,16 @@ import { NextPage } from "next";
 import Layout from "@components/layout";
 import { Suspense, lazy } from "react";
 import Loading from "@components/loading";
+import Slogan from "../components/slogan";
+import ProductsList from "../components/ProductsList";
 
-const ProductsList = lazy(() => import("../components/ProductsList"));
+// const ProductsList = lazy(() => import("../components/ProductsList"));
 
 const Home: NextPage = () => {
   return (
     <Layout hasTabBar mainTitle seoTitle="이웃과 함께하는" pathName="Product">
+      <Slogan />
       <Suspense fallback={<Loading />}>
-        <div className="px-4 py-4 ">
-          <div className="bg-gradient-to-tr from-blue-500 via-sky-500 to-sky-400 text-white p-5 rounded-md space-y-10  shadow-violet-400 shadow-md">
-            <div className="text-2xl">이웃과 함께하는 비투게더</div>
-            <div>
-              <div>공동구매, 중고거래, 친목도모 등</div>
-              <div>다양하게 활용해 보세요!</div>
-            </div>
-          </div>
-        </div>
         <ProductsList />
       </Suspense>
     </Layout>
