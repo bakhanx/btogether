8340d6c0e925bdf69cc06aa10f87a8d8b135038f
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-11-21 17:59:18 +0900

    글쓰기 버튼 label 추가

--- components/floatingButton.tsx
+++ components/floatingButton.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 type FloatingButton = {
   pathName: "Product" | "Story" | "Chat" | "Profile";
-}
+};
 
 export default function FloatingButton({ pathName }: FloatingButton) {
   return (
     <div className="relative bottom-24 z-40 flex max-w-screen-xl   justify-center border-none">
       {pathName === "Product" && (
-        <Link href={"product/upload"}>
+        <Link href={"product/upload"} aria-label="글쓰기">
           <div className="bottom-24 flex h-16 w-16 cursor-pointer items-center justify-center rounded-full border-0 border-transparent bg-gradient-to-t from-cyan-500 via-blue-500 to-purple-500 text-white shadow-xl transition-colors  hover:from-cyan-600 hover:via-blue-600 hover:to-purple-600">
             <svg
               className="h-6 w-6"
