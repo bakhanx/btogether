144b23ffc15e35e183788c6d18e36eaaef579e40
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-12-08 17:27:48 +0900

    import client

--- pages/api/products/[id]/delete.ts
+++ pages/api/products/[id]/delete.ts
@@ -1,6 +1,7 @@
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { withApiSession } from "@libs/server/withSession";
 import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
 
 async function handler(
   req: NextApiRequest,
