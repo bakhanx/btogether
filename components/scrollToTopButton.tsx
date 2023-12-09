239456c1fe3624889971bce3f7bc6a21c116ed45
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-12-09 23:05:26 +0900

    bottomTab 유무 버튼 위치 상하변경

--- components/scrollToTopButton.tsx
+++ components/scrollToTopButton.tsx
@@ -1,9 +1,16 @@
-export default function ScrollToTopButton() {
+import { cls } from "@libs/client/utils";
+
+export default function ScrollToTopButton({ hasBottomTab = true }) {
   const scrollToTop = () => {
     window.scrollTo(0, 0);
   };
   return (
-    <div className="relative right-6  bottom-28 border-none">
+    <div
+      className={cls(
+        hasBottomTab ? "bottom-28" : "bottom-28",
+        "fixed right-6 border-none"
+      )}
+    >
       <div
         onClick={scrollToTop}
         className="flex h-10 w-10  cursor-pointer items-center justify-center rounded-full border-0 bg-gray-200 font-bold shadow-sm hover:bg-gray-300"
