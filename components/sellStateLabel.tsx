a06e8237ee03ef2f10452b5f99c45259440a7afa
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-01-08 18:51:53 +0900

    판매상태 라벨 변경

--- components/sellStateLabel.tsx
+++ components/sellStateLabel.tsx
@@ -1,29 +1,50 @@
+import { cls } from "@libs/client/utils";
 import { SellingType } from "types/product";
 
 export type sellStateProps = {
   sellState: SellingType;
-  hideSelling?: boolean;
+  hideLabel?: boolean;
+};
+
+enum parseSellType {
+  "selling" = "판매중",
+  "sold" = "판매완료",
+  "reserve" = "예약중",
 }
 
-const SellStateLabel = ({ sellState, hideSelling }: sellStateProps) => {
+const SellStateLabel = ({ sellState, hideLabel }: sellStateProps) => {
   return (
     <>
-      {sellState === "reserve" && (
+        <span
+          className={cls(
+            "mr-1 rounded-full p-[3px] text-[10px] leading-4 text-white font-bold",
+            sellState === "selling"
+              ? "bg-blue-700 hidden"
+              : sellState === "sold"
+              ? "bg-orange-500 hidden"
+              : sellState === "reserve"
+              ? "bg-purple-600 "
+              : ""
+          )}
+        >
+          {parseSellType[sellState]}
+        </span>
+      {/* {sellState === "reserve" && (
         <span className=" mr-1 rounded-sm bg-green-700 p-1 text-xs text-white">
           예약중
         </span>
       )}
-      {sellState === "sold" && (
+      {! hideLabel && sellState === "sold" && (
         <span className=" mr-1 rounded-sm bg-gray-600 p-1 text-xs text-white">
-          거래완료
+          완료
         </span>
       )}
 
-      {!hideSelling && sellState === "selling" && (
+      {!hideLabel && sellState === "selling" && (
         <span className=" mr-1 rounded-sm bg-violet-700 p-1 text-xs text-white">
-          거래중
+          진행중
         </span>
-      )}
+      )} */}
     </>
   );
 };
