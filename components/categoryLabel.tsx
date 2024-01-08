3f86e4034a0370a2f96f9ede0cd457ab8648394c
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-01-08 22:34:38 +0900

    디자인 변경

--- components/categoryLabel.tsx
+++ components/categoryLabel.tsx
@@ -3,7 +3,7 @@ import { Category } from "@prisma/client";
 
 export type CategoryProps = {
   category: Category;
-  hideLabel?: boolean;
+  large?: boolean;
 };
 
 enum parseCategory {
@@ -12,19 +12,20 @@ enum parseCategory {
   "Gather" = "모임",
 }
 
-const CategoryLabel = ({ category, hideLabel }: CategoryProps) => {
+const CategoryLabel = ({ category, large = false }: CategoryProps) => {
   return (
     <>
       <span
         className={cls(
-          "mr-1 rounded-sm p-1 text-xs text-white ",
+          "mr-1 rounded-sm  text-white ",
           category === Category.Product
             ? "bg-blue-600"
             : category === Category.Free
             ? "bg-orange-600"
             : category === Category.Gather
-            ? "bg-green-600 " 
-            : ""
+            ? "bg-green-600 "
+            : "",
+          large ? "p-3 text-sm" : "p-1 text-xs"
         )}
       >
         {parseCategory[category]}
