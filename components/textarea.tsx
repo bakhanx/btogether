960badeca43c4c71f439edd56828ce12103e6e75
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-01-09 21:48:28 +0900

    스토리 textarea 색상 변경

--- components/textarea.tsx
+++ components/textarea.tsx
@@ -3,6 +3,7 @@ import { UseFormRegisterReturn } from "react-hook-form";
 type TextAreaProps = {
   label?: string;
   name?: string;
+  color?: string;
   register: UseFormRegisterReturn;
   [key: string]: any;
 }
@@ -11,6 +12,7 @@ export default function TextArea({
   label,
   name,
   register,
+  color="blue",
   ...rest
 }: TextAreaProps) {
   return (
@@ -27,7 +29,7 @@ export default function TextArea({
       <textarea
         id={name}
         {...register}
-        className="mt-1 w-full resize-none rounded-md border border-gray-300 p-1 shadow-sm focus:border-blue-500 focus:placeholder-transparent focus:ring-blue-500"
+        className={`mt-1 w-full resize-none rounded-md border-2 border-gray-300 p-1 shadow-sm focus:border-${color}-500 focus:placeholder-transparent focus:ring-${color}-500 focus:outline-none`}
         rows={4}
         {...rest}
       />
