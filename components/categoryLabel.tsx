e16874d7db8a91615f811b1ad42c3e86d86e74d6
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-01-09 21:47:44 +0900

    스토리 카테고리 라벨 추가

--- components/categoryLabel.tsx
+++ components/categoryLabel.tsx
@@ -1,8 +1,10 @@
 import { cls } from "@libs/client/utils";
 import { Category } from "@prisma/client";
+import { StoryCategory } from "./categoryButton";
 
 export type CategoryProps = {
-  category: Category;
+  routeType: "Product" | "Story";
+  category: Category | StoryCategory;
   large?: boolean;
 };
 
@@ -10,26 +12,55 @@ enum parseCategory {
   "Product" = "판매",
   "Free" = "나눔",
   "Gather" = "모임",
+  "Daily" = "일상",
+  "Review" = "후기",
+  "Info" = "정보",
+  "Ask" = "질문",
 }
 
-const CategoryLabel = ({ category, large = false }: CategoryProps) => {
+const CategoryLabel = ({
+  routeType,
+  category,
+  large = false,
+}: CategoryProps) => {
   return (
     <>
-      <span
-        className={cls(
-          "mr-1 rounded-sm  text-white ",
-          category === Category.Product
-            ? "bg-blue-600"
-            : category === Category.Free
-            ? "bg-orange-600"
-            : category === Category.Gather
-            ? "bg-green-600 "
-            : "",
-          large ? "p-3 text-sm" : "p-1 text-xs"
-        )}
-      >
-        {parseCategory[category]}
-      </span>
+      {routeType === "Product" && (
+        <span
+          className={cls(
+            "mr-1 rounded-sm  text-white ",
+            category === "Product"
+              ? "bg-blue-600"
+              : category === "Free"
+              ? "bg-orange-600"
+              : category === "Gather"
+              ? "bg-green-600 "
+              : "",
+            large ? "p-3 text-sm" : "p-1 text-xs"
+          )}
+        >
+          {parseCategory[category]}
+        </span>
+      )}
+      {routeType === "Story" && (
+        <span
+          className={cls(
+            "ml-3.5 rounded-full px-2.5 py-0.5 font-bold text-white ",
+            category === "Daily"
+              ? "bg-violet-600"
+              : category === "Review"
+              ? "bg-green-600"
+              : category === "Info"
+              ? "bg-blue-600 "
+              : category === "Ask"
+              ? "bg-orange-600 "
+              : "",
+            large ? "p-3 text-sm" : "p-1 text-xs"
+          )}
+        >
+          {parseCategory[category]}
+        </span>
+      )}
     </>
   );
 };
