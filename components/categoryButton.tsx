49451e2d3d83e2e7ddd4ef16ea027dfd147558d0
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-01-09 21:47:36 +0900

    스토리 카테고리 추가

--- components/categoryButton.tsx
+++ components/categoryButton.tsx
@@ -2,13 +2,14 @@ import { cls } from "@libs/client/utils";
 import { useState } from "react";
 
 export type ProductCategory = "Product" | "Free" | "Gather";
+export type StoryCategory = "Daily" | "Review" | "Info" | "Ask";
 
 type CategoryButtonType = {
   text: string;
   onClick?: React.MouseEventHandler;
-  value?: ProductCategory;
+  value?: ProductCategory | StoryCategory;
   [key: string]: any;
-  color : "blue" | "orange"
+  color: "blue" | "orange" | "red" | "green" | "violet";
 };
 
 export default function CategoryButton({
@@ -22,8 +23,8 @@ export default function CategoryButton({
     <button
       {...rest}
       className={cls(
-        category === value ? `border-${color}-500` : "border-gray-300",
-        `rounded-sm  p-2 text-sm border-2 hover:border-${color}-500`
+        category === value ? `border-${color}-600` : "border-gray-300",
+        `rounded-sm  p-2 text-sm border-2 hover:border-${color}-600`
       )}
       value={value}
     >
