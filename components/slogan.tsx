0ca1e42905f0d795bd3afda09d42fb6aa08f539e
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-01-09 17:35:26 +0900

    px 간격 조정

--- components/slogan.tsx
+++ components/slogan.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 
 const Slogan: NextPage = () => {
   return (
-    <div className="px-4 py-4 ">
+    <div className="px-2 py-4 ">
       <div className="bg-gradient-to-tr from-blue-500 via-sky-500 to-sky-400 text-white p-5 rounded-md space-y-10  shadow-violet-400 shadow-md">
         <div className="text-2xl">이웃과 함께하는 비투게더</div>
         <div>
