15cb28d85f0c2610b5045351e72ee4fec64c27f4
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-01-09 17:36:27 +0900

    favorite 단일 요청

--- pages/api/users/me/records.ts
+++ pages/api/users/me/records.ts
@@ -3,6 +3,7 @@ import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 import { withApiSession } from "@libs/server/withSession";
 import { Kind } from "@prisma/client";
+import { CategoryType } from "@components/myProductList";
 
 async function handler(
   req: NextApiRequest,
@@ -11,57 +12,95 @@ async function handler(
   if (req.method === "GET") {
     const {
       session: { user },
-      query: { kind },
+      query: { kind, category },
     } = req;
-    console.log(kind);
-    const records = await client.record.findMany({
-      where: {
-        userId: user?.id,
-        kind: kind as Kind,
-      },
-      include: {
-        product: {
-          include: {
-            _count: {
-              select: {
-                records: {
-                  where: {
-                    kind: {
-                      equals: "Favorite"
+
+    if (kind === "Favorite") {
+      const records = await client.record.findMany({
+        where: {
+          userId: user?.id,
+          kind: "Favorite",
+        },
+        include: {
+          product: {
+            include: {
+              _count: {
+                select: {
+                  records: {
+                    where: {
+                      kind: {
+                        equals: "Favorite",
+                      },
                     },
                   },
+                  chatRooms: true,
                 },
-                chatRooms: true,
               },
             },
           },
         },
-      },
-    });
+        orderBy: {
+          id: "desc",
+        },
+      });
 
-    res.json({
-      ok: true,
-      records,
-    });
+      res.json({
+        ok: true,
+        records,
+      });
+    } else {
+      const records = await client.record.findMany({
+        where: {
+          userId: user?.id,
+          kind: kind as Kind,
+          product: {
+            category: category as CategoryType,
+          },
+        },
+        include: {
+          product: {
+            include: {
+              _count: {
+                select: {
+                  records: {
+                    where: {
+                      kind: {
+                        equals: "Favorite",
+                      },
+                    },
+                  },
+                  chatRooms: true,
+                },
+              },
+            },
+          },
+        },
+      });
+
+      res.json({
+        ok: true,
+        records,
+      });
+    }
   }
 
-  if (req.method === "POST") {
-    const {
-      session: { user },
-      query: { kind },
-    } = req;
+  // if (req.method === "POST") {
+  //   const {
+  //     session: { user },
+  //     query: { kind },
+  //   } = req;
 
-    const isExistRecord = Boolean(
-      await client.record.findFirst({
-        where: {},
-      })
-    );
-  }
+  //   const isExistRecord = Boolean(
+  //     await client.record.findFirst({
+  //       where: {},
+  //     })
+  //   );
+  // }
 }
 
 export default withApiSession(
   withHandler({
-    methods: ["GET", "POST"],
+    methods: ["GET"],
     handler,
     isPrivate: true,
   })
