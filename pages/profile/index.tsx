3d87cf827c2c33a4e95e12cb336b8c2da964480c
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-01-09 17:38:14 +0900

    UI 수정

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -122,26 +122,26 @@ const UserInfo = () => {
             <div className="text-gray text-sm font-medium ">판매내역</div>
           </div>
         </Link>
-        {/* 모집내역 */}
-        <Link href="/profile/gather ">
-          <div className="flex items-center py-3 gap-x-2 hover:bg-gray-100">
+        {/* 구매내역 */}
+        <Link href="/profile/purchase">
+          <div className="flex items-center  py-3 gap-x-2 hover:bg-gray-100">
             <div className="text-blue-500">
               <svg
-                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
                 fill="none"
-                viewBox="0 0 24 24"
                 stroke="currentColor"
-                className="w-6 h-6"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="2"
-                  d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 1 1 0-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 0 1-1.44-4.282m3.102.069a18.03 18.03 0 0 1-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 0 1 8.835 2.535M10.34 6.66a23.847 23.847 0 0 0 8.835-2.535m0 0A23.74 23.74 0 0 0 18.795 3m.38 1.125a23.91 23.91 0 0 1 1.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 0 0 1.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 0 1 0 3.46"
-                />
+                  d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+                ></path>
               </svg>
             </div>
-            <div className="text-gray text-sm font-medium ">모집내역</div>
+            <span className="text-gray text-sm font-medium">구매내역</span>
           </div>
         </Link>
 
@@ -169,26 +169,26 @@ const UserInfo = () => {
         </Link>
         <div className=" w-full h-1 bg-gradient-to-r to-blue-500 from-purple-500 opacity-20" />
 
-        {/* 구매내역 */}
-        <Link href="/profile/purchase">
-          <div className="flex items-center  py-3 gap-x-2 hover:bg-gray-100">
+        {/* 모집내역 */}
+        <Link href="/profile/gather ">
+          <div className="flex items-center py-3 gap-x-2 hover:bg-gray-100">
             <div className="text-blue-500">
               <svg
-                className="h-6 w-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="w-6 h-6"
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="2"
-                  d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
-                ></path>
+                  d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 1 1 0-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 0 1-1.44-4.282m3.102.069a18.03 18.03 0 0 1-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 0 1 8.835 2.535M10.34 6.66a23.847 23.847 0 0 0 8.835-2.535m0 0A23.74 23.74 0 0 0 18.795 3m.38 1.125a23.91 23.91 0 0 1 1.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 0 0 1.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 0 1 0 3.46"
+                />
               </svg>
             </div>
-            <span className="text-gray text-sm font-medium">구매내역</span>
+            <div className="text-gray text-sm font-medium ">모집내역</div>
           </div>
         </Link>
         {/* 참여내역 */}
