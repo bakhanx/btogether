74445b397ffc5cce57fca7a02194ed0febab5184
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-01-10 13:31:20 +0900

    카테고리 constants 생성

---
+++ constants/category.ts
@@ -0,0 +1,41 @@
+export const ProductCategoryList = [
+  {
+    category: "Product",
+    color: "blue",
+    text: "상품",
+  },
+  {
+    category: "Free",
+    color: "green",
+    text: "나눔",
+  },
+  {
+    category: "Gather",
+    color: "orange",
+    text: "모임",
+  },
+];
+
+export const StoryCategoryList = [
+  {
+    category: "Daily",
+    color: "purple",
+    text: "일상",
+  },
+
+  {
+    category: "Review",
+    color: "blue",
+    text: "후기",
+  },
+  {
+    category: "Info",
+    color: "green",
+    text: "정보",
+  },
+  {
+    category: "Ask",
+    color: "orange",
+    text: "질문",
+  },
+];
