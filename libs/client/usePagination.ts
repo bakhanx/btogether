2f61cca3fc55d66467c0693e4dcfd3b406e1132b
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-01-11 23:00:15 +0900

    Mobile innerHeight 버그로 인한 편차보정

--- libs/client/usePagination.ts
+++ libs/client/usePagination.ts
@@ -1,15 +1,32 @@
 import { useEffect, useState } from "react";
 
+// 기기 편차 - 보정값
+const D = 100;
+
 export const usePagination = () => {
   const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleScroll = () => {
     const scrollHeight = document.documentElement.scrollHeight;
     const scrollTop = document.documentElement.scrollTop;
     const clientHeight = window.innerHeight;
-    if (scrollTop + clientHeight >= scrollHeight) {
-      setPage((prev) => prev + 1);
+    if (scrollTop + clientHeight >= scrollHeight - D) {
+      // setPage((prev) => prev + 1);
+      setIsLoading(true);
     }
   };
+
+  // Mobile innerHeight 에러로 인한 임시코드
+  useEffect(() => {
+    if (isLoading) {
+      setTimeout(() => {
+        setPage((prev) => prev + 1);
+        setIsLoading(false);
+        console.log(page);
+      }, 10);
+    }
+  }, [isLoading, page]);
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
@@ -18,7 +35,7 @@ export const usePagination = () => {
       window.removeEventListener("scroll", handleScroll);
       window.removeEventListener("touchMove", handleScroll);
     };
-  }, [page]);
+  }, []);
 
   return page;
 };
