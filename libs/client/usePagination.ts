af8b57775b4e4dfe4145aedb1a9ab294788e7cf5
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-01-11 21:26:09 +0900

    [모바일] doc.clientHeight -> window.innerHeight

--- libs/client/usePagination.ts
+++ libs/client/usePagination.ts
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 
 export const usePagination = () => {
+  const [isloading, setIsLoading] = useState(false);
   const [page, setPage] = useState(1);
   const handleScroll = () => {
     const scrollHeight = document.documentElement.scrollHeight;
     const scrollTop = document.documentElement.scrollTop;
-    const clientHeight = document.documentElement.clientHeight;
+    const clientHeight = window.innerHeight;
     if (scrollTop + clientHeight >= scrollHeight) {
       setPage((prev) => prev + 1);
     }
@@ -13,10 +14,12 @@ export const usePagination = () => {
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
+    window.addEventListener("touchMove", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("touchMove", handleScroll);
     };
-  }, [page, setPage]);
+  }, [page]);
 
   return page;
 };
