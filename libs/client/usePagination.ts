4b3568392f463128c2d01c282c78eafbff872cec
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-12-13 13:14:38 +0900

    Refactor: 페이지 조건 추가

--- libs/client/usePagination.ts
+++ libs/client/usePagination.ts
@@ -1,31 +1,32 @@
 import { useEffect, useState } from "react";
 
 // 기기 편차 - 보정값
-const D = 100;
+const D = 300;
 
-export const usePagination = () => {
+export const usePagination = (totalPages: number) => {
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
-
   const handleScroll = () => {
     const scrollHeight = document.documentElement.scrollHeight;
     const scrollTop = document.documentElement.scrollTop;
     const clientHeight = window.innerHeight;
-    if (scrollTop + clientHeight >= scrollHeight - D) {
-      // setPage((prev) => prev + 1);
+    if (
+      scrollTop + clientHeight >= scrollHeight &&
+      !isLoading &&
+      page < totalPages
+    ) {
       setIsLoading(true);
     }
   };
 
-  // Mobile innerHeight 에러로 인한 임시코드
   useEffect(() => {
-    if (isLoading) {
+    if (isLoading && page < totalPages) {
       setTimeout(() => {
         setPage((prev) => prev + 1);
         setIsLoading(false);
       }, 300);
     }
-  }, [isLoading, page]);
+  }, [isLoading, page, totalPages]);
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
